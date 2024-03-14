c3b43c52b69412b3ee516d855460c3fa514c0b49
Mohammed Aly <90681796+MohammedAly22@users.noreply.github.com>
2024-03-14 03:01:56 +0200

    Update utils.py

--- Demo/utils.py
+++ Demo/utils.py
@@ -11,6 +11,19 @@ from weaviate.embedded import EmbeddedOptions
 
 
 def get_wikipedia_article(query):
+    """
+    Fetch a Wikipedia article based on a query.
+
+    Parameters:
+        - query (str): The query to search for on Wikipedia.
+
+    Returns:
+        - tuple: A tuple containing the content of the Wikipedia article and a status code.
+        If the query is ambiguous, the status code is -1 and a message with possible options is returned.
+        If no Wikipedia page is found, the status code is -2 and a message indicating this is returned.
+        If the query is successful, the status code is 1 and the content of the article is returned.
+    """
+    
     try:
         page = wikipedia.page(query)
         return page.content, 1
@@ -23,6 +36,16 @@ def get_wikipedia_article(query):
 
 
 def load_documents(file_name):
+    """
+    Load documents from a file using the `TextLoader` from langchain.
+
+    Parameters:
+        - file_name (str): The name of the file to load documents from.
+
+    Returns:
+        - list: A list of documents loaded from the file.
+    """
+    
     loader = TextLoader(file_name, encoding='utf8')
     documents = loader.load()
 
@@ -30,6 +53,16 @@ def load_documents(file_name):
 
 
 def split_to_chunks(documents):
+    """
+    Split documents into chunks using the `CharacterTextSplitter` from langchain.
+
+    Parameters:
+        - documents (list): A list of documents to split into chunks.
+
+    Returns:
+        - list: A list of chunks created from the documents.
+    """
+    
     text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     chunks = text_splitter.split_documents(documents)
 
@@ -37,6 +70,17 @@ def split_to_chunks(documents):
 
 
 def retrieve(inference_api_key, chunks):
+    """
+    Retrieve embeddings for chunks using `HuggingFaceInferenceAPIEmbeddings` and Weaviate.
+
+    Parameters:
+        - inference_api_key (str): The API key for HuggingFace's inference API.
+    -     chunks (list): A list of chunks to retrieve embeddings for.
+
+    Returns:
+        - Weaviate.as_retriever: A retriever object that can be used to retrieve similar chunks.
+    """
+    
     embeddings = HuggingFaceInferenceAPIEmbeddings(
     api_key=inference_api_key, model_name="sentence-transformers/all-MiniLM-l6-v2")
 
@@ -53,6 +97,17 @@ def retrieve(inference_api_key, chunks):
 
 
 def prepare_instruction(context, answer):
+    """
+    Prepare an instruction prompt for a question-answering task.
+
+    Parameters:
+        - context (str): The context from which to generate the question.
+        - answer (str): The answer to highlight in the question.
+
+    Returns:
+        - str: The instruction prompt for the question-answering task.
+    """
+    
     context_splits = context.split(answer)
     
     text = ""
