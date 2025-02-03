bca37f147305f6040f7e92ccb300339ebdeedb6e
Mohammed Aly <90681796+MohammedAly22@users.noreply.github.com>
2025-02-03 02:45:42 +0200

    Update Question_Generation_without_RAG.py

--- Demo/Question_Generation_without_RAG.py
+++ Demo/Question_Generation_without_RAG.py
@@ -1,38 +1,97 @@
 import streamlit as st
-from utils import pipe_v1, pipe_v2, prepare_instruction
+from utils import pipe, get_wikipedia_article, load_documents, split_to_chunks, retrieve
 
-
-with open('Demo/style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.title("Question Generation without RAG")
-with st.form('Generation Form'):
-    selected_model = st.selectbox(label='Select a Model:', options=['T5-Small-V2', 'T5-Small-V1'])
-    context = st.text_area(label='Enter Your Context: ', placeholder='Please, enter a context to generate question from', height=200)
-    answer = st.text_input(label='Enter Your Answer:', placeholder='Please, enter an answer snippet from the provided context')
-    num_of_questions = st.number_input(
-        label='Enter a Number of Generated Questions:',
-        placeholder='Please, enter a number of generated questions you need',
-        min_value=1,
-        max_value=5)
-    
-    submitted = st.form_submit_button('Generate')
+# inference_api_key = "#"
+inference_api_key = st.text_input("Enter HF Pass")
+
+# Initialize session state
+if "option" not in st.session_state:
+    st.session_state["option"] = "Enter a context"
+
+
+def update_session_state():
+    st.session_state["option"] = st.session_state["hidden_option"]
+
+
+option = st.selectbox(
+    "select an option",
+    ("Enter a context", "Enter a topic"),
+    key="hidden_option",
+    on_change=update_session_state,
+)
+selected_option = None
+
+with open("./style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+with st.form("submission_form"):
+    if st.session_state["option"] == "Enter a context":
+        context = st.text_area("Enter your context", height=300)
+        selected_option = "context"
+
+        number_of_questions = st.number_input(
+            f"Choose number of questions", min_value=1, max_value=5
+        )
+
+    else:
+        topic = st.text_input("Enter your topic")
+        selected_option = "topic"
+
+        number_of_questions = st.number_input(
+            f"Choose number of questions", min_value=1, max_value=5
+        )
+
+    submitted = st.form_submit_button("Submit")
 
     if submitted:
-        if context:
-            if answer:
-                prompt = prepare_instruction(context, answer)
+        if selected_option == "context":
+            if context != "":
                 with st.spinner(f"Generating Questions..."):
-                    if selected_model == 'T5-Small-V1':
-                        generated_output = pipe_v1(prompt, num_return_sequences=num_of_questions, num_beams=5, num_beam_groups=5, diversity_penalty=1.0)
-                    else:
-                        generated_output = pipe_v2(prompt, num_return_sequences=num_of_questions, num_beams=5, num_beam_groups=5, diversity_penalty=1.0)
-                        
-                st.write('Generated Question(s):')
+                    generated_output = pipe(
+                        context,
+                        num_return_sequences=number_of_questions,
+                        num_beams=5,
+                        num_beam_groups=5,
+                        diversity_penalty=1.0,
+                    )
+                st.write("Generated Question")
                 for i, item in enumerate(generated_output):
                     st.info(f"Question #{i+1}: {item['generated_text']}")
             else:
-                st.error('Please, provide an answer snippet')
-        
+                st.error("Please, Enter a Context to Generate From")
         else:
-            st.error('Please, provide a context to generate questions from')
+            if topic != "":
+                article_text, success_code = get_wikipedia_article(topic)
+
+                if success_code == 1:
+                    st.success(f"{topic} File Reterieved Successfully!")
+
+                    with open(f"{topic}.txt", "w", encoding="utf-8") as file:
+                        file.write(article_text)
+
+                    with st.spinner(f"Generating Questions..."):
+                        documents = load_documents(f"{topic}.txt")
+                        chunks = split_to_chunks(documents)
+                        retriever = retrieve(inference_api_key, chunks)
+                        context = retriever.get_relevant_documents(topic)[
+                            0
+                        ].page_content
+                        # st.write(f'Retrieved Context')
+                        # st.info(context)
+                        generated_output = pipe(
+                            context,
+                            num_return_sequences=number_of_questions,
+                            num_beams=5,
+                            num_beam_groups=5,
+                            diversity_penalty=1.0,
+                        )
+
+                    st.write("Generated Question")
+                    for i, item in enumerate(generated_output):
+                        st.info(f"Question #{i+1}: {item['generated_text']}")
+                else:
+                    st.error(article_text)
+            else:
+                st.error("Please, Enter a Topic to Retrieve a File About")
