d64e7565f1b59be91c20aba70a6ad3e26d139a5e
Mohammed Aly <90681796+MohammedAly22@users.noreply.github.com>
2025-02-03 02:50:58 +0200

    Update Question_Generation_without_RAG.py

--- Demo/Question_Generation_without_RAG.py
+++ Demo/Question_Generation_without_RAG.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from utils import pipe, get_wikipedia_article, load_documents, split_to_chunks, retrieve
 
-with open("/style.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# with open("/style.css") as f:
+#     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 st.title("Question Generation without RAG")
